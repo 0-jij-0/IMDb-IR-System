3c5c051b92ae0472c1f63cb171e95e0940b624e5
0-jij-0 <jihadghanna@gmail.com>
2021-05-15 23:51:42 +0300

    Optimized Spelling Correction Algorithm

--- IMDb-IR-System/SpellingCorrection.h
+++ IMDb-IR-System/SpellingCorrection.h
@@ -3,13 +3,43 @@
 #include "Indexing.h"
 #include "BooleanModel.h"
 
-vector<set<string>> alternatives;
-map<char, int> characterMap;
+vector<map<char, int>> characterMap(5);
+vector<map<string, vector<int>>*> index = { &nameIndex, &actorIndex, &characterIndex, &yearIndex, &ratingIndex };
+
+#define NAME_INDEX 0
+#define ACTOR_INDEX 1
+#define CHARACTER_INDEX 2
+#define YEAR_INDEX 3
+#define RATING_INDEX 4
+
 
 struct TrieNode {
 	bool b = false; vector<TrieNode*> nextNode;
-	TrieNode() : nextNode(characterMap.size(), nullptr) {}
+	TrieNode(int i) : nextNode(characterMap[i].size(), nullptr) {}
 };
+
+void insertWord(TrieNode* root, const string& s, const int& i) {
+	TrieNode* cur = root; for (const char& c : s) {
+		int idx = characterMap[i][c];
+		if (cur->nextNode[idx] == nullptr)
+			cur->nextNode[idx] = new TrieNode(i);
+		cur = cur->nextNode[idx];
+	}
+	cur->b = true;
+}
+
+vector<TrieNode*> tries;
+
+void initializeTries() {
+	for (int i = 0; i < 5; i++) {
+		for (auto& x : *index[i]) for (auto& y : x.first) characterMap[i][y];
+		int id = 0; for (auto& x : characterMap[i]) x.second = id++;
+
+		tries.push_back(new TrieNode(i)); 
+		for (auto& x : *index[i]) insertWord(tries[i], x.first, i);
+	}
+}
+
 
 struct PQObj {
 	vector<int> r; string word = "";
@@ -17,56 +47,39 @@ struct PQObj {
 	bool operator < (const PQObj& rhs) const { return r.size() > rhs.r.size(); }
 };
 
-void insertWord(TrieNode* root, const string& s) {
-	TrieNode* cur = root; for (const char& c : s) {
-		int idx = characterMap[c];
-		if (cur->nextNode[idx] == nullptr)
-			cur->nextNode[idx] = new TrieNode();
-		cur = cur->nextNode[idx];
-	}
-	cur->b = true;
-}
-
-TrieNode* initializeTrie(const map<string, vector<int>>& index) {
-	for (auto& x : index) for (const char& y : x.first) characterMap[y];
-	int id = 0; for (auto& x : characterMap) x.second = id++;
-
-	TrieNode* root = new TrieNode();
-	for (auto& x : index) insertWord(root, x.first);
-	return root;
-}
-
-void findAlternatives(const string& original, string& cur, TrieNode* curNode, const int& originalID, int i = 0, int ed = 2) {
+void findAlternatives(const string& original, string& cur, TrieNode* curNode, const int& ID, 
+	const int& j, vector<set<string>> &alternatives, int i = 0, int ed = 2) {
 	if (i == (int)original.size()) {
-		if (ed == 0) { if (curNode->b) alternatives[originalID].insert(cur); return; }
-		findAlternatives(original, cur, curNode, originalID, i, ed - 1);
-		for (auto& c : characterMap) {
+		if (ed == 0) { if (curNode->b) alternatives[ID].insert(cur); return; }
+		findAlternatives(original, cur, curNode, ID, j, alternatives, i, ed - 1);
+		for (auto& c : characterMap[j]) {
 			if (curNode->nextNode[c.second] != nullptr) {
 				cur.push_back(c.first);
-				findAlternatives(original, cur, curNode->nextNode[c.second], originalID, i, ed - 1);
+				findAlternatives(original, cur, curNode->nextNode[c.second], ID, j, alternatives, i, ed - 1);
 				cur.pop_back();
 			}
 		} return;
 	}
 
 	if (ed == 0) { //Advance to the next character
-		if (curNode->nextNode[characterMap[original[i]]] == nullptr) { return; }
+		if (curNode->nextNode[characterMap[j][original[i]]] == nullptr) { return; }
 		cur.push_back(original[i]);
-		findAlternatives(original, cur, curNode->nextNode[characterMap[original[i]]], originalID, i + 1, 0);
+		findAlternatives(original, cur, curNode->nextNode[characterMap[j][original[i]]], ID, j, alternatives, i + 1, 0);
 		cur.pop_back(); return;
 	}
 
-	findAlternatives(original, cur, curNode, originalID, i, ed - 1); //Discard an operation
-	findAlternatives(original, cur, curNode, originalID, i + 1, ed - 1); //Delete current character
-	for(auto &c : characterMap) if (curNode->nextNode[c.second] != nullptr) {
+	findAlternatives(original, cur, curNode, ID, j, alternatives, i, ed - 1); //Discard an operation
+	findAlternatives(original, cur, curNode, ID, j, alternatives, i + 1, ed - 1); //Delete current character
+	for(auto &c : characterMap[j]) if (curNode->nextNode[c.second] != nullptr) {
 		cur.push_back(c.first); bool dif = c.first != original[i];
-		findAlternatives(original, cur, curNode->nextNode[c.second], originalID, i, ed - 1); //Insert Character
-		findAlternatives(original, cur, curNode->nextNode[c.second], originalID, i + 1, ed - dif); //Replace Character
+		findAlternatives(original, cur, curNode->nextNode[c.second], ID, j, alternatives, i, ed - 1); //Insert Character
+		findAlternatives(original, cur, curNode->nextNode[c.second], ID, j, alternatives, i + 1, ed - dif); //Replace Character
 		cur.pop_back();
 	}
 }
 
-void generatePhrases(priority_queue<PQObj>& top3, vector<int> curList, string curP, map<string, vector<int>> &index, int i = 1) {
+void generatePhrases(priority_queue<PQObj>& top3, vector<int> curList, string curP, 
+	map<string, vector<int>> &curIndex, vector<set<string>> &alternatives, int i = 1) {
 	if (curList.empty()) { return; }
 	if (i == alternatives.size()) {
 		if ((int)top3.size() < 3) { top3.push(PQObj(curList, curP)); return; }
@@ -76,19 +89,18 @@ void generatePhrases(priority_queue<PQObj>& top3, vector<int> curList, string cu
 	}
 
 	for (auto& x : alternatives[i])
-		generatePhrases(top3, listIntersection(curList, index[x]), curP + x + " ", index, i + 1);
+		generatePhrases(top3, listIntersection(curList, curIndex[x]), curP + x + " ", curIndex, alternatives, i + 1);
 }
 
-vector<int> spellingCorrection(const string& query, map<string, vector<int>>& index) {
-	TrieNode* root = initializeTrie(index);
+vector<int> spellingCorrection(const string& query, int t) {
 	stringstream ss(query);	string word, curFA; int id = 0;
-	while (ss >> word) {
+	vector<set<string>> alternatives; while (ss >> word) {
 		curFA = ""; alternatives.emplace_back();
-		findAlternatives(word, curFA, root, id++);
+		findAlternatives(word, curFA, tries[t], id++, t, alternatives);
 	}
 
 	priority_queue<PQObj> top3; for (auto& x : alternatives[0])
-		generatePhrases(top3, index[x], x + " ", index);
+		generatePhrases(top3, (*index[t])[x], x + " ", *index[t], alternatives);
 
 	vector<PQObj> suggestions; while (!top3.empty()) {
 		suggestions.push_back(top3.top()); top3.pop();
--- IMDb-IR-System/Test.cpp
+++ IMDb-IR-System/Test.cpp
@@ -5,8 +5,9 @@
 
 int main() {
 	initializeSearchEngine();
+	initializeTries();
 	string q = "cram hemsworth";
-	vector<int> res = spellingCorrection(q, actorIndex);
+	vector<int> res = spellingCorrection(q, ACTOR_INDEX);
 	for (auto& x : res) cout << x << ' '; cout << '\n';
 	cin.ignore(2); return 0;
 }
